9d6fb4c5d6db06ab0bdc1719eb6b5c79b41d98c1
MaceeQi <maceeqi@gmail.com>
2023-04-10 09:38:37 -0400

    prompt user to log in to MySQL before directing user to community rentals application

---
+++ main.py
@@ -0,0 +1,46 @@
+import pymysql
+
+# Prompt user for MySQL username and password
+def prompt_user():
+    username = input("MySQL username: ")
+    password = input("MySQL password: ")
+
+    return username, password
+
+
+# Prompt user to connect to database using MySQL username/password
+def connect_to_database(database):
+    # prompt user for MySQL username and password
+    username, password = prompt_user()
+
+    try:
+        # instantiate connection object to database
+        connection = pymysql.connect(host='localhost',
+                                     user=username,
+                                     password=password,
+                                     db=database,
+                                     charset='utf8mb4',
+                                     cursorclass=pymysql.cursors.DictCursor)
+
+        print("\nYou have successfully logged in to MySQL!")
+        return connection
+
+    except pymysql.err.OperationalError as e:
+        print('\nError: %d: %s' % (e.args[0], e.args[1]))
+        print('\nPlease enter a valid MySQL username and password to access the database.')
+
+
+if __name__ == '__main__':
+    database = "community_rentals"
+    connection = None
+
+    # prompt user for valid username/password to connect to database
+    print("\nPlease enter your MySQL username and password to access the database.")
+    while (connection == None):
+        connection = connect_to_database(database)
+
+    print("\nDirecting you to the Community Rentals Application...")
+
+
+    # close connection
+    connection.close()
